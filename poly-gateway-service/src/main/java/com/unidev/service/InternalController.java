09ec74d18b6b8ab4d1a9f2a35e7f1ba297257096
Denis O <denis@universal-development.com>
2017-02-15 22:06:53 +0200

    Added controllers readme description

--- poly-gateway-service/src/main/java/com/unidev/service/InternalController.java
+++ poly-gateway-service/src/main/java/com/unidev/service/InternalController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Controller for "internal" operations usage
+ */
 @RestController
 public class InternalController {
 
