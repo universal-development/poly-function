02890c69c7e602672873b49a031b07bc9628e464
Denis O <denis@universal-development.com>
2017-02-15 21:49:32 +0200

    Added example public api

--- poly-gateway-service/src/main/java/com/unidev/service/PublicService.java
+++ poly-gateway-service/src/main/java/com/unidev/service/PublicService.java
@@ -4,12 +4,12 @@ import com.unidev.platform.j2ee.common.WebUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController("/public")
+@RestController
 public class PublicService {
 
     private static Logger LOG = LoggerFactory.getLogger(PublicService.class);
@@ -20,7 +20,7 @@ public class PublicService {
     @Autowired
     private WebUtils webUtils;
 
-    @PostMapping
+    @GetMapping("/api/public")
     public String handle() {
         LOG.info("Public request {} {}", webUtils.listAllHeaders(servletRequest),  servletRequest);
         return "Public page";
