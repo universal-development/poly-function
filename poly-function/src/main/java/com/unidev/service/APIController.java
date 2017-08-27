5c21dde3585cb445d0c606b300b4812c375b9ab0
Denis O <denis@universal-development.com>
2017-08-27 20:09:41 +0300

    Updated API controller

--- poly-function/src/main/java/com/unidev/service/APIController.java
+++ poly-function/src/main/java/com/unidev/service/APIController.java
@@ -1,21 +1,22 @@
 package com.unidev.service;
 
 import com.unidev.platform.j2ee.common.WebUtils;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Public exposed service endpoint
  */
 @RestController
-public class PublicController {
+@RequestMapping("/api/v1")
+public class APIController {
 
-    private static Logger LOG = LoggerFactory.getLogger(PublicController.class);
+    private static Logger LOG = LoggerFactory.getLogger(APIController.class);
 
     @Autowired
     private HttpServletRequest servletRequest;
@@ -23,7 +24,7 @@ public class PublicController {
     @Autowired
     private WebUtils webUtils;
 
-    @GetMapping("/api/public")
+    @GetMapping("public")
     public String handle() {
         LOG.info("Public request {} {}", webUtils.listAllHeaders(servletRequest),  servletRequest);
         return "Public page";
