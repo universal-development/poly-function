0d6db6db16c10aafec319de128a394e97f1008c7
Denis O <denis@universal-development.com>
2017-09-16 09:36:33 +0300

    Added registration of JMX bean on management interface

--- poly-function/src/main/java/com/unidev/polyfunction/Application.java
+++ poly-function/src/main/java/com/unidev/polyfunction/Application.java
@@ -1,19 +1,14 @@
 package com.unidev.polyfunction;
 
 import com.unidev.platform.j2ee.common.WebUtils;
-import org.jminix.console.servlet.MiniConsoleServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application implements ServletContextInitializer {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -23,11 +18,6 @@ public class Application implements ServletContextInitializer {
 	public WebUtils webUtils() {
 		return new WebUtils();
 	}
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        servletContext.addServlet("JmxMiniConsoleServlet", MiniConsoleServlet.class).addMapping("/jmx/*");
-    }
 
 }
 
---
+++ poly-function/src/main/java/com/unidev/polyfunction/ManagementConfig.java
@@ -0,0 +1,17 @@
+package com.unidev.polyfunction;
+
+import org.jminix.console.servlet.MiniConsoleServlet;
+import org.springframework.boot.actuate.autoconfigure.ManagementContextConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@ManagementContextConfiguration
+public class ManagementConfig {
+    @Bean
+    ServletRegistrationBean jmxServletRegistration () {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new MiniConsoleServlet());
+        servletRegistrationBean.addUrlMappings("/jmx/*");
+        return servletRegistrationBean;
+    }
+}
