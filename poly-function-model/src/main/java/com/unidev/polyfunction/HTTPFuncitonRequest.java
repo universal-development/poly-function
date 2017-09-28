c849f23592b9a9f3fafa095019d4a9724259f6fd
Denis O <denis@universal-development.com>
2017-09-28 23:42:53 +0300

    Added generic response

--- poly-function-model/src/main/java/com/unidev/polyfunction/HTTPFuncitonRequest.java
+++ poly-function-model/src/main/java/com/unidev/polyfunction/HTTPFuncitonRequest.java
@@ -3,13 +3,17 @@ package com.unidev.polyfunction;
 
 import java.util.HashMap;
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HTTPFuncitonRequest {
 
     private String script;
