08361a39f0cad3fa441a570256e66c3601be63af
Denis O <denis@universal-development.com>
2017-09-28 22:49:33 +0300

    Script evaluation service

---
+++ poly-function/src/main/java/com/unidev/polyfunction/evaluator/ScriptEvaluator.java
@@ -0,0 +1,12 @@
+package com.unidev.polyfunction.evaluator;
+
+/**
+ * Service for evaluation of scripts
+ */
+public interface ScriptEvaluator {
+
+    boolean canHandle(String scriptName);
+
+
+
+}
