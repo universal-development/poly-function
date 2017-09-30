5023f17bc2a719824c4bff9f43b58b4c3d1b560c
Denis O <denis@universal-development.com>
2017-09-30 12:50:11 +0300

    API controller for function evaluation

--- poly-function/src/main/java/com/unidev/polyfunction/controller/APIController.java
+++ poly-function/src/main/java/com/unidev/polyfunction/controller/APIController.java
@@ -1,11 +1,14 @@
 package com.unidev.polyfunction.controller;
 
 import com.unidev.platform.j2ee.common.WebUtils;
+import com.unidev.polyfunction.HTTPFunctionRequest;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +27,8 @@ public class APIController {
     @Autowired
     private WebUtils webUtils;
 
-    @GetMapping("public")
-    public String handle() {
+    @PostMapping("function")
+    public String handle(@RequestBody HTTPFunctionRequest httpFunctionRequest) {
         LOG.info("Public request {} {}", webUtils.listAllHeaders(servletRequest),  servletRequest);
         return "Public page";
     }
